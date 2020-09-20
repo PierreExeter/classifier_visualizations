80488c6e0ec73330b7eb8a54b27044470acea600
PierreExeter <pierre.aumjaud@gmail.com>
2020-09-20 22:26:51 +0200

    first commit

---
+++ 2_simple_plot_KNN.py
@@ -0,0 +1,65 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_moons
+from sklearn.neighbors import KNeighborsClassifier
+
+
+# CREATE DATASET, SCALE IT and SPLIT INTO TRAIN AND TEST SET
+X, y = make_moons(n_samples=100, noise=0.3, random_state=0)
+X = StandardScaler().fit_transform(X)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.4, random_state=42)
+
+# DEFINE CLASSIFIER, FIT AND COMPUTE METRICS
+name_clf = "KNeighborsClassifier"
+clf = KNeighborsClassifier(3)
+clf.fit(X_train, y_train)
+train_accuracy = clf.score(X_train, y_train)
+test_accuracy = clf.score(X_test, y_test)
+train_str = str(round(train_accuracy, 2))
+test_str = str(round(test_accuracy, 2))
+
+# DEFINE MESH GRID
+h = .02  # step size of the mesh
+x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                     np.arange(y_min, y_max, h))
+
+# PREDICT PROBABILITY CLASSIFIER
+Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+Z = Z.reshape(xx.shape)
+
+# DEFINE FIGURE AND COLORMAP
+fig, ax = plt.subplots(figsize=(10, 10))
+cm = plt.cm.viridis
+first, last = cm.colors[0], cm.colors[-1]
+cm_bright = ListedColormap([first, last])
+
+# PLOT CONTOUR CLASSIFIER, DECISION BOUNDARY, TRAIN AND TEST DATA
+cb = ax.contourf(xx, yy, Z, levels=10, cmap=cm, alpha=0.8)
+ax.contour(xx, yy, Z, levels=[.5], colors='k', linestyles='dashed', linewidths=2)
+ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright, edgecolors='k', linewidth=2, label="Train data")
+ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, edgecolors='k', alpha=0.6, label="Test data")
+
+# COLORBAR, AXIS LIMITS, TITLE
+fs = 15
+cbar = plt.colorbar(cb, ticks=[0, 1])
+cbar.ax.set_yticklabels(['0', '1'], fontsize=fs)
+ax.legend(fontsize=fs, loc='upper left')
+ax.set_xlim(xx.min(), xx.max())
+ax.set_ylim(yy.min(), yy.max())
+ax.set_xlabel("$X_1$")
+ax.set_ylabel("$X_2$")
+ax.set_xticks(())
+ax.set_yticks(())
+title_str = name_clf+' | Train accuracy: '+str(train_str)+' | Test accuracy: '+test_str
+ax.set_title(title_str, fontsize=fs)
+
+# SAVE FIGURE
+plt.tight_layout()
+# plt.show()
+plt.savefig('plots/simple_plot_KNN.png', dpi=200, bbox_inches='tight')
---
+++ README.md
@@ -0,0 +1,32 @@
+# Scikit-learn classifer visualization
+
+Some binary classifiers are trained on a 2D dataset and visualised.
+
+
+# Installation
+
+```bash
+git clone ...
+```
+
+add conda env.
+
+## Classifier comparison
+
+This popular visualisation by Scikit Learn compare the performance of various classifier.
+Adapted from [here](https://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html).
+
+
+```bash
+python 1_classifier_comparison.py
+```
+
+<img src="plots/classifier_comparison.png"/>
+
+## K-Nearest Neighbors plot
+
+```bash
+python 2_simple_plot_KNN.py
+```
+
+<img src="plots/simple_plot_KNN.png"/>
